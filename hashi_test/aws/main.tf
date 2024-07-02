865f0c904efb12a80a1deb279c99238a1c53deca
tkaatzEviden <167749301+tkaatzEviden@users.noreply.github.com>
2024-07-03 00:08:15 +0200

    Add NAT Gateway

--- hashi_test/aws/main.tf
+++ hashi_test/aws/main.tf
@@ -42,6 +42,7 @@ module "alb" {
       cidr_ipv4   = "0.0.0.0/0"
     }
   }
+
   security_group_egress_rules = {
     all = {
       ip_protocol = "-1"
@@ -130,6 +131,10 @@ module "vpc" {
   azs             = local.azs
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  one_nat_gateway_per_az = false
 
   tags = local.tags
 }
