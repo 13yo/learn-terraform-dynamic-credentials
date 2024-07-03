3c4d35eac2f8b30ae6af39abfe8b79dbcbb6ded1
tkaatzEviden <167749301+tkaatzEviden@users.noreply.github.com>
2024-07-03 16:27:40 +0200

    Combined AWS and Google Cloud

--- hashi_test/main.tf
+++ hashi_test/main.tf
@@ -10,7 +10,7 @@ module "aws" {
 module "google_cloud" {
     source = "./gcp"
 
-    gcp_project_id = "hashitest-44562752345"
+    gcp_project_id = "hashitest-427520"
     network_name = "acme-hashi-test"
     region = "us-west1"
 }
